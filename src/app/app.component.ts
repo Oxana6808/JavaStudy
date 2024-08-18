d4785ff0c7c8f82d7d6e9e2da7b70d742af3107d
OksanaDmitrieva <oxana_dmitrieva@bk.ru>
2024-08-19 02:23:35 +0400

    обавила menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone: true,  // Делает компонент standalone
-  imports: [CommonModule, HeaderComponent]  // Добавьте нужные модули и компоненты
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, MenuComponent]  // Убедитесь, что MenuComponent здесь присутствует
 })
 export class AppComponent {
   title = 'JavaStudy';
